1480e2f7a84011c767321b9c290743757f3ae19d
GUHAN S <gguhanr@gmail.com>
2025-09-21 09:57:32 +0530

    Add files via upload

---
+++ Java Math/negateExact.java
@@ -0,0 +1,8 @@
+class nagateExact{
+    public static void main(String[] args) {
+        int a=10;
+        int b=-9;
+        System.out.println(Math.negateExact(a));
+        System.out.println(Math.negateExact(b));
+    }
+}
